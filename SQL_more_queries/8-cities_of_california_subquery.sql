657c23fc852e3e4675b99d72e39c180bf896070e
elioraaa <elioraqendro2021@gmail.com>
2026-02-26 21:26:05 +0100

    FILE 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -3,8 +3,6 @@
 SELECT id, name
 FROM cities
 WHERE state_id = (
-    SELECT id
-    FROM states
-    WHERE name = 'California'
-);
+    SELECT id FROM states WHERE name = 'California'
+)
 ORDER BY id ASC;
