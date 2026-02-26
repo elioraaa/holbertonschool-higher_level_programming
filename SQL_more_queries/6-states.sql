5f800016a7f1a25e3d7453e2d0f9cde2aa55fa94
elioraaa <elioraqendro2021@gmail.com>
2026-02-26 21:16:09 +0100

    FILE 6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- script create a data base and a table within
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
     name VARCHAR(256) NOT NULL
