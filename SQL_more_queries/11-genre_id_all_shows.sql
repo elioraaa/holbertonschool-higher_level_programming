d49c5026db61e5e29303c0506a49d60571ba7bd6
elioraaa <elioraqendro2021@gmail.com>
2026-02-27 12:35:06 +0100

    FILEs 9 10 11 12

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- script lists all shows with their genre IDs (or NULL if none) from hbtn_0d_tvshows
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
